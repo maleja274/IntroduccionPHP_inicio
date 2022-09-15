1650eaca278d1a01bc66747077a2a16ca97ebc39
maleja274 <maleja274@hotmail.com>
2022-09-14 22:18:53 -0500

    funcion.echo.var_dump

---
+++ 01-hola-mundo.php
@@ -0,0 +1,18 @@
+<?php include 'includes/header.php';
+
+echo "<h1>Hola Mundito</h1>"; // LAS QUE MAS SE UTILIZAN EN LAS FUNCIONES DE PHP SON ECHO Y VAR_DUMP.
+
+echo "Hola mundo";
+
+echo("Hola mundo");
+
+print "Hola Mundote";
+
+print ("Hola Mundote");
+
+print_r("HOLA MUNDO");
+
+var_dump("HOLI MUNDI");
+
+
+include 'includes/footer.php';
